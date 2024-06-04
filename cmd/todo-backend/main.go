3e9dbf61869222348b8cb0f0dcb21f9572ddea30
Yosiah de Koeyer <dev@yosiahdekoeyer.dev>
2024-06-04 21:04:45 +1000

    Jank REST backend for TODO

--- cmd/todo-backend/main.go
+++ cmd/todo-backend/main.go
@@ -1,7 +1,102 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Todo struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+}
+
+var todos []Todo
 
 func main() {
-	fmt.Println("This is the TODO REST backend service.")
+	router := mux.NewRouter()
+
+	router.HandleFunc("/todos", getTodos).Methods("GET")
+	router.HandleFunc("/todos/{id}", getTodo).Methods("GET")
+	router.HandleFunc("/todos", createTodo).Methods("POST")
+	router.HandleFunc("/todos/{id}", updateTodo).Methods("PUT")
+	router.HandleFunc("/todos/{id}", deleteTodo).Methods("DELETE")
+
+	log.Println("Starting on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func getTodos(w http.ResponseWriter, r *http.Request) {
+	log.Println("Getting all todos")
+	w.Header().Set("Content-Type", "application/json")
+	_ = json.NewEncoder(w).Encode(todos)
+}
+
+func getTodo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	id, _ := strconv.Atoi(params["id"])
+
+	log.Println("Getting todo with id", id)
+
+	for _, todo := range todos {
+		if todo.ID == id {
+			_ = json.NewEncoder(w).Encode(todo)
+			return
+		}
+	}
+	_ = json.NewEncoder(w).Encode(&Todo{})
+}
+
+func createTodo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var todo Todo
+	_ = json.NewDecoder(r.Body).Decode(&todo)
+	todo.ID = len(todos) + 1
+
+	log.Println("Creating todo with id", todo.ID)
+
+	todos = append(todos, todo)
+	_ = json.NewEncoder(w).Encode(todo)
+}
+
+// Update todo
+func updateTodo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	id, _ := strconv.Atoi(params["id"])
+
+	log.Println("Updating todo with id", id)
+
+	var updatedTodo Todo
+	_ = json.NewDecoder(r.Body).Decode(&updatedTodo)
+
+	for index, todo := range todos {
+		if todo.ID == id {
+			todos[index] = updatedTodo
+			break
+		}
+	}
+
+	_ = json.NewEncoder(w).Encode(updatedTodo)
+}
+
+func deleteTodo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	id, _ := strconv.Atoi(params["id"])
+
+	log.Println("Deleting todo with id", id)
+
+	for index, todo := range todos {
+		if todo.ID == id {
+			todos = append(todos[:index], todos[index+1:]...)
+			break
+		}
+	}
+
+	_ = json.NewEncoder(w).Encode(todos)
 }
