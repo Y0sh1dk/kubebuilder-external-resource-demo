990752cac7b994995a91d5ff8152abe496eb52d5
Yosiah de Koeyer <dev@yosiahdekoeyer.dev>
2024-06-04 21:31:06 +1000

    Reconcile triggering

--- internal/controller/todo_controller.go
+++ internal/controller/todo_controller.go
@@ -22,7 +22,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
+	logger "sigs.k8s.io/controller-runtime/pkg/log"
 
 	externalresourcedevv1alpha1 "github.com/Y0sh1dk/kubebuilder-external-resource-demo/api/v1alpha1"
 )
@@ -37,19 +37,10 @@ type TodoReconciler struct {
 //+kubebuilder:rbac:groups=external-resource.dev.external-resource.dev,resources=todoes/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=external-resource.dev.external-resource.dev,resources=todoes/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Todo object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.17.3/pkg/reconcile
 func (r *TodoReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := logger.FromContext(ctx)
 
-	// TODO(user): your logic here
+	log.Info("Reconciling Todo", "Name", req.NamespacedName, "Namespace", req.Namespace)
 
 	return ctrl.Result{}, nil
 }
